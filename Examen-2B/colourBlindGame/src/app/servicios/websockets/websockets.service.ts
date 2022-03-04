2a1971efb10b959568e3e32b65cc09828d191d3a
Carolina Mendoza <carolina.mendoza@epn.edu.ec>
2022-03-04 15:40:29 -0500

    configuracion websockets en el cliente

---
+++ Examen-2B/colourBlindGame/src/app/servicios/websockets/websockets.service.ts
@@ -0,0 +1,11 @@
+import {Injectable} from '@angular/core';
+import {Socket} from 'ngx-socket-io';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WebsocketsService {
+  constructor(private socket: Socket) {
+  }
+}
