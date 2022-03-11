a71eadd0675f4eebb791fd08dce93577f020ddb7
Cesar <cesar.leon03@hotmail.com>
2022-03-10 22:11:41 -0500

    Conexión Firebase

--- wavan-leon-mendoza-proyecto-main/compra-venta-autos/src/app/componentes-auth/dashboard/dashboard.component.ts
+++ wavan-leon-mendoza-proyecto-main/compra-venta-autos/src/app/componentes-auth/dashboard/dashboard.component.ts
@@ -1,18 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from "../../servicios/auth/auth.service";
 
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import {addDoc, collection} from "@angular/fire/firestore";
+import {environment} from "../../../environments/environment";
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-
+  app = initializeApp(environment.firebase);
+  db = getFirestore();
   constructor(
     public authService: AuthService
   ) { }
 
   ngOnInit(): void {
   }
+
 
 }
