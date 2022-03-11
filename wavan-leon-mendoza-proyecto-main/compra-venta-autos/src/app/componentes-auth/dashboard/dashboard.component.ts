35532598195a16a072f6b7ed49384a1d630d35b2
Cesar <cesar.leon03@hotmail.com>
2022-03-10 23:37:54 -0500

    Pantalla principal

--- wavan-leon-mendoza-proyecto-main/compra-venta-autos/src/app/componentes-auth/dashboard/dashboard.component.ts
+++ wavan-leon-mendoza-proyecto-main/compra-venta-autos/src/app/componentes-auth/dashboard/dashboard.component.ts
@@ -3,8 +3,10 @@ import {AuthService} from "../../servicios/auth/auth.service";
 
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import {addDoc, collection} from "@angular/fire/firestore";
+import {addDoc, collection, getDocs} from "@angular/fire/firestore";
 import {environment} from "../../../environments/environment";
+import firebase from "firebase/compat";
+import DocumentData = firebase.firestore.DocumentData;
 
 @Component({
   selector: 'app-dashboard',
@@ -14,12 +16,52 @@ import {environment} from "../../../environments/environment";
 export class DashboardComponent implements OnInit {
   app = initializeApp(environment.firebase);
   db = getFirestore();
+
+  //--
+  anios: number[] =[]
+  tipo :DocumentData[] =[]
+  marca :DocumentData[] =[]
+  vehiculos :DocumentData[] =[]
+  anioFiltro: number =2022;
   constructor(
-    public authService: AuthService
+    public authService: AuthService,
   ) { }
 
   ngOnInit(): void {
+    this.obtenerTipo()
+    this.obtenerMarca()
+    this.obtenerAnios()
+    this.obtenerVehiculos()
   }
 
+  async obtenerTipo(){
+    let tipoCol = collection(this.db, 'tipo');
+    let tipoSnapshot = await getDocs(tipoCol);
+    this.tipo = tipoSnapshot.docs.map(doc => doc.data());
+  }
 
+  async obtenerMarca(){
+    let marcaCol = collection(this.db, 'marca');
+    let marcaSnapshot = await getDocs(marcaCol);
+    this.marca = marcaSnapshot.docs.map(doc => doc.data());
+  }
+  obtenerAnios(){
+    for (var i = 1; i<50 ; i++){
+      this.anios.push(i)
+    }
+    console.log(this.anios, 'asdasdasd')
+  }
+  async obtenerVehiculos(){
+    let vehCol = collection(this.db, 'vehiculos');
+    let vehSnapshot = await getDocs(vehCol);
+    this.vehiculos = vehSnapshot.docs.map(doc => doc.data());
+  }
+
+  filtrarCategoria(catElement:string) {
+    console.log('aasdasdad')
+  }
+
+  filtrarAnio(anio: number) {
+    this.anioFiltro = anio
+  }
 }
